94f82a43bb4c33c435eae8cf7305629015ecc70a
Андрей Варачев <dartanyun@yandex.com>
2024-04-22 21:58:21 +0500

    Создана кастомная модель User
    Нововведения:
    В моделе пользователя реализованы только поля username и password (на мой взгляд больше и не нужно);
    К моделе был создан и подключен кастомный менеджер для обработки операций по созданию пользователей;
    Модель пользователя была выведена в админ панель;

--- ecom/users/admin.py
+++ ecom/users/admin.py
@@ -1,3 +1,18 @@
-# from django.contrib import admin
+from django.contrib import admin
+from django.contrib.auth.hashers import make_password
+from users.models import User
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    """
+    Админ панель модели пользователей.
+    """
+
+    list_display = ("username", "is_active", "is_superuser")
+    search_fields = ("username",)
+
+    def save_model(self, request, obj, form, change):
+        """Хэширует пароль и сохраняет его в базе данных"""
+        obj.password = make_password(obj.password)
+        super().save_model(request, obj, form, change)

4511689029b6c492751e12f0c5224629776e8467
Андрей Варачев <dartanyun@yandex.com>
2024-04-23 00:07:32 +0500

    Добавлены приложение аутентификации, подключен api_v1, д

--- ecom/ecom/settings.py
+++ ecom/ecom/settings.py
@@ -43,9 +43,13 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
+    "rest_framework.authtoken",
+    "djoser",
     # проектные приложения
     "users",
     "stock",
+    "api_v1",
 ]
 
 MIDDLEWARE = [

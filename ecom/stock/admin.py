c9591f64b0d0f18edd6b5c92d1bda0a58a6c5d00
Андрей Варачев <dartanyun@yandex.com>
2024-04-22 22:00:22 +0500

    Модели Category, Stock, Equipment
    Нововведения:
    Реализована базовая структура базы данных для проекта в которую входят модели Category, Stock, Equipment;
    Модели были выведены в админку;

--- ecom/stock/admin.py
+++ ecom/stock/admin.py
@@ -1,3 +1,28 @@
-# from django.contrib import admin
+from django.contrib import admin
+from stock.models import Category, Equipment, Stock
 
-# Register your models here.
+
+@admin.register(Equipment)
+class EquipmentAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "name",
+        "category",
+        "stock",
+        "description",
+        "quantity",
+    )
+    search_fields = ("name", "category", "stock")
+    list_filter = ("name", "category", "stock")
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "description")
+    search_fields = ("name",)
+
+
+@admin.register(Stock)
+class StockAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "address", "description")
+    search_fields = ("name", "address")
--- ecom/stock/models.py
+++ ecom/stock/models.py
@@ -1,3 +1,32 @@
-# from django.db import models
+from core.models import NameDescriptionModel
+from django.db import models
 
-# Create your models here.
+
+class Category(NameDescriptionModel):
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+
+
+class Stock(NameDescriptionModel):
+    address = models.CharField(max_length=200, verbose_name="Адрес")
+
+    class Meta:
+        verbose_name = "Склад"
+        verbose_name_plural = "Склады"
+
+
+class Equipment(NameDescriptionModel):
+    category = models.ForeignKey(
+        Category, on_delete=models.CASCADE, verbose_name="Категория"
+    )
+    stock = models.ForeignKey(
+        Stock, on_delete=models.CASCADE, verbose_name="Склад"
+    )
+    quantity = models.PositiveIntegerField(
+        default=0, verbose_name="Количество"
+    )
+
+    class Meta:
+        verbose_name = "Оборудование"
+        verbose_name_plural = "Оборудования"
